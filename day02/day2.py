36df08b45c49865072fca664ae5678344e0253b7
hallelazulai <hallel.azulai@weizmann.ac.il>
2025-11-02 17:17:25 +0200

    day2

---
+++ day02/day2.py
@@ -0,0 +1,10 @@
+class Circle:
+    def __init__(self, R):
+        if R <= 0:
+            raise ValueError("RADIUS MUST BE POSITIVE")
+        self.RADIUS = float(R)
+
+    def Area(self):
+        return 3.14 * (self.RADIUS ** 2)
+
+print("AREA:", Circle(2).Area())
